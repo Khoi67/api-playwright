2cbd07959197430962d4ca012ceb6fa816588fdf
트란하/CJ올리브네트웍스/Tran Thanh Nha <tran.nha@amorepacific.com>
2025-12-30 15:02:38 +0700

    Api testing

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-"# api-playwright" 
+# Playwright API Automation – Booking Service
+
+- This project is an **API automation testing framework** built with **Playwright Test and TypeScript** to validate the **Restful Booker API**.  
+- It covers a full **CRUD flow** and follows real-world API automation best practices.
+- [API Demo Website](https://restful-booker.herokuapp.com/apidoc/index.html)
+
+
+## Tech Stack
+- Playwright Test  
+- TypeScript  
+- Node.js  
+- RESTful API Testing  
+
+
+
+## Test Coverage
+
+| No | Endpoint | Method | Description |
+|----|----------|--------|-------------|
+| 1 | `/auth` | POST | Generate authentication token |
+| 2 | `/booking` | GET | Get all booking IDs |
+| 3 | `/booking` | POST | Create a new booking |
+| 4 | `/booking/{id}` | GET | Get booking details by ID |
+| 5 | `/booking/{id}` | PUT | Update booking information |
+| 6 | `/booking/{id}` | PATCH | Partial update booking |
+| 7 | `/booking/{id}` | DELETE | Delete booking |
+
+
+
+## How to Run Tests
+
+### 1. Install dependencies
+```bash
+npm install
+```
+### 2. Run test
+```bash
+npx playwright test
--- tests/api/booking.spec.ts
+++ tests/api/booking.spec.ts
@@ -1,17 +1,21 @@
 import { expect, test } from '@playwright/test';
-import { AUTH_PAYLOAD, BASE_URL } from '../../utils/TestData';
+import { AUTH_PAYLOAD, BASE_URL, NEW_BOOKING } from '../../utils/TestData';
 
-test.describe('Booking API Tests', () => {
-    let token;
+test.describe.serial('Booking API Tests', () => {
+    let token: string;
+    let bookingId: number;
 
     test('1. Auth - CreateToken', async ({ request }) => {
         const response = await request.post(`${BASE_URL}/auth`, {
             data: {
                 username: AUTH_PAYLOAD.username,
                 password: AUTH_PAYLOAD.password
+            },
+            headers: {
+                'Content-Type': 'application/json'
             }
         });
-        
+
         expect(response.status()).toBe(200);
 
         const responseBody = await response.json();
@@ -23,8 +27,116 @@ test.describe('Booking API Tests', () => {
     test('2.All Booking - GetBookingIds', async ({ request }) => {
         const response = await request.get(`${BASE_URL}/booking`);
         expect(response.status()).toBe(200);
+
         const bookings = await response.json();
         expect(Array.isArray(bookings)).toBe(true);
-        console.log('Bookings: ', bookings);
+    });
+
+    test('4. Create Booking - CreateBooking', async ({ request }) => {
+        const respone = await request.post(`${BASE_URL}/booking`, {
+            data: {
+                firstname: NEW_BOOKING.firstname,
+                lastname: NEW_BOOKING.lastname,
+                totalprice: NEW_BOOKING.totalprice,
+                depositpaid: NEW_BOOKING.depositpaid,
+                bookingdates: {
+                    checkin: NEW_BOOKING.bookingdates.checkin,
+                    checkout: NEW_BOOKING.bookingdates.checkout
+                },
+                additionalneeds: NEW_BOOKING.additionalneeds
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+
+        expect(respone.status()).toBe(200);
+        const bookingResponse = await respone.json();
+        expect(bookingResponse).toHaveProperty('bookingid');
+        expect(bookingResponse).toHaveProperty('booking');
+
+        bookingId = bookingResponse.bookingid;
+        // console.log('New Booking Created with ID:', bookingResponse.bookingid);
+    });
+    test('3. A Booking - GetBooking', async ({ request }) => {
+        const response = await request.get(`${BASE_URL}/booking/${bookingId}`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        expect(response.status()).toBe(200);
+
+        const booking = await response.json();
+        expect(booking).toHaveProperty('firstname');
+        expect(booking).toHaveProperty('lastname');
+        expect(booking).toHaveProperty('totalprice');
+        expect(booking).toHaveProperty('depositpaid');
+        expect(booking).toHaveProperty('bookingdates');
+        expect(booking).toHaveProperty('additionalneeds');
+        // console.log('Booking Details:', booking);
+    });
+
+    test('5. Booking - UpdateBooking', async ({ request }) => {
+        const response = await request.put(`${BASE_URL}/booking/${bookingId}`, {
+            data: {
+                firstname: 'Khoi',
+                lastname: 'Updated',
+                totalprice: 250,
+                depositpaid: false,
+                bookingdates: {
+                    checkin: '2025-02-01',
+                    checkout: '2025-02-10'
+                },
+                additionalneeds: 'Late Checkout'
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': `token=${token}`,
+                'Authorization': `Basic ${token}`
+            }
+        })
+        expect(response.status()).toBe(200);
+
+        const updatedBooking = await response.json();
+        expect(updatedBooking.firstname).toBe('Khoi');
+        expect(updatedBooking.lastname).toBe('Updated');
+        expect(updatedBooking.totalprice).toBe(250);
+        expect(updatedBooking.depositpaid).toBe(false);
+        // console.log('Updated Booking Details:', updatedBooking);
+    });
+
+    test('6. Booking - PartialUpdateBooking', async ({ request }) => {
+        const response = await request.patch(`${BASE_URL}/booking/${bookingId}`, {
+            data: {
+                firstname: 'Khoi',
+                lastname: 'Partially - Updated'
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': `token=${token}`,
+                'Authorization': `Basic ${token}`
+            }
+        })
+        expect(response.status()).toBe(200);
+
+        const partialUpdatedBooking = await response.json();
+        expect(partialUpdatedBooking.firstname).toBe('Khoi');
+        expect(partialUpdatedBooking.lastname).toBe('Partially - Updated');
+        // console.log('Partially Updated Booking Details:', partialUpdatedBooking);
+    });
+
+    test('7. Booking - DeleteBooking', async ({ request }) => {
+        const response = await request.delete(`${BASE_URL}/booking/${bookingId}`, {
+            headers: {
+                'Cookie': `token=${token}`,
+                'Authorization': `Basic ${token}`
+            }
+        });
+
+        // Tài liệu đang để sai 
+        expect(response.status()).toBe(201);
     });
 });
